d87c16f697bc59695b3f487cd235fc16f8b2d08f
lialivehi <111819496+lialivehi@users.noreply.github.com>
2022-09-26 13:02:35 -1000

    Initial Commit

---
+++ AppBuildOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppBuildOne
+//
+//  Created by Marilia Ledezma on 9/26/22.
+//  Copyright © 2022 Marilia Ledezma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
